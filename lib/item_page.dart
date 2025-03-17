8304b366044cad8a3a94f79a52ffeeffe6d13ed7
Igor Augusto <igoraugusto1029@gmail.com>
2025-03-16 22:30:23 -0300

    Adiciona validação no textFormField

--- lib/item_page.dart
+++ lib/item_page.dart
@@ -9,50 +9,71 @@ class ItemPage extends StatelessWidget {
 
   const ItemPage({super.key, required this.title, this.item});
 
+  void _saveItem(BuildContext context, TodoItem newItem) {
+    var todoProvider = Provider.of<TodoProvider>(context, listen: false);
+
+    if (item != null) {
+      todoProvider.updateItem(item!, newItem);
+    } else {
+      todoProvider.addItem(newItem);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _controller = TextEditingController(text: item?.title);
+    final TextEditingController _controller =
+        TextEditingController(text: item?.title);
+    final _formItemPageKey = GlobalKey<FormState>();
 
-    return Consumer<TodoProvider>(
-      builder: (context, todoProvider, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(child: Column(
-              children: [
-                TextField(
-                  controller: _controller,
-                  autofocus: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Título',
-                  ),
-                  onSubmitted: (value) {
-                    todoProvider.addItem(TodoItem(title: value));
-                    Navigator.pop(context);
-                  },
-                )
-              ],
-            )),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              if (item != null){
-                todoProvider.updateItem(item!, TodoItem(title: _controller.text));
-              } else {
-                todoProvider.addItem(TodoItem(title: _controller.text));
-              }
+    return Consumer<TodoProvider>(builder: (context, todoProvider, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+              key: _formItemPageKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _controller,
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Título',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira um título';
+                      }
+
+                      return null;
+                    },
+                    onFieldSubmitted: (value) {
+                      if (_formItemPageKey.currentState!.validate()) {
+                        _saveItem(context, TodoItem(title: _controller.text));
+                        Navigator.pop(context);
+                      }
+                    },
+                  )
+                ],
+              )),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_formItemPageKey.currentState!.validate()) {
+              _saveItem(context, TodoItem(title: _controller.text));
               Navigator.pop(context);
-            },
-            tooltip: "Salvar",
-            child: Icon(Icons.check,),
+            }
+          },
+          tooltip: "Salvar",
+          child: Icon(
+            Icons.check,
           ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
 }
