91d0faffbdfaff8771ea270041318fe866da010e
Igor <igoraugusto1029@gmail.com>
2025-05-17 15:41:02 -0300

    Adiciona firebase

--- lib/main.dart
+++ lib/main.dart
@@ -87,7 +87,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ToDo',
       debugShowCheckedModeBanner: Foundation.kDebugMode,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
